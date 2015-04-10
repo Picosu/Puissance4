4dc63224667b7058b9c8fac7dc72cb4e1ac5c271
Maxence de Cussac <Picsou@iMac-de-Maxence.bouyguesbox.fr>
2015-04-10 08:48:19 +0200

    début des dessins complet

--- js/Token.js
+++ js/Token.js
@@ -24,7 +24,6 @@ function drawToken (token, color) {
 	if (!color) {
 		color = "#fbdd12";
 	}
-
 	// Don't forget those dimension to avoid circle to be messed up
 	canvas.width = 150;
 	canvas.height = 600;
@@ -37,9 +36,9 @@ function drawToken (token, color) {
 	
 	context.beginPath();
 	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-	context.clip();
-	context.beginPath();
-	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+	//context.clip();
+	//context.beginPath();
+	//context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
 
 	context.fillStyle = color;
 
@@ -49,6 +48,17 @@ function drawToken (token, color) {
 	context.stroke();
 
 	context.closePath();
+}
+
+function drawAllToken(tokens) {
+	for (var row = 0; row <= rows; row++)  {
+		for (var column = 0; column <= columns; column++) {
+			var token = tokens[row + ',' + column] || 0;
+			if (token) {
+				drawToken(tokens[row + ',' + column], tokens[row + ',' + column].color);
+			}
+		}
+	}
 }
 
 window.requestAnimFrame = (function(callback) {
@@ -79,34 +89,34 @@ $('#add-player').on('submit', function (e) {
 
 function animate(myCircle, canvas, context, startTime, nbTokens) {
 	// update
+	drawAllToken(tokens);
+	var time = (new Date()).getTime() - startTime;
+	var index = myCircle.y%canvas.height;
+	var linearSpeed = 500;
+	// pixels / second
+	var newY = linearSpeed * time / 1000;
+	var limitY = canvas.height - (nbTokens * 2 * myCircle.radius) - myCircle.radius;
+	if(newY < limitY) {
+		isAnimated = true;
 
-		var time = (new Date()).getTime() - startTime;
-		var index = myCircle.y%canvas.height;
-		var linearSpeed = 500;
-		// pixels / second
-		var newY = linearSpeed * time / 1000;
-		var limitY = canvas.height - (nbTokens * 2 * myCircle.radius) - myCircle.radius;
-		if(newY < limitY) {
-			isAnimated = true;
+		myCircle.y = newY;
 
-			myCircle.y = newY;
+		drawToken(myCircle);
 
-			drawToken(myCircle);
+		// request new frame
+		requestAnimFrame(function() {
+			animate(myCircle, canvas, context, startTime, nbTokens);
+		});
 
-			// request new frame
-			requestAnimFrame(function() {
-				animate(myCircle, canvas, context, startTime, nbTokens);
-			});
-
-		} else {
-			// Fin de l'animation. Rajout de la position de l'objet dans la case associée.
-			var y = Math.ceil(myCircle.y /(2*myCircle.radius));
-			console.log(y);
-			tokens[myCircle.column + ',' + y] = myCircle;
-			isAnimated = false;
-			myCircle.y = limitY;
-			drawToken(myCircle);
-		}
+	} else {
+		// Fin de l'animation. Rajout de la position de l'objet dans la case associée.
+		var y = Math.ceil(myCircle.y /(2*myCircle.radius));
+		console.log(y);
+		tokens[myCircle.column + ',' + y] = myCircle;
+		isAnimated = false;
+		myCircle.y = limitY;
+		drawToken(myCircle);
+	}
 }
 
 var tokens = {};
@@ -119,12 +129,16 @@ function drawTokenForCanvas(x) {
 		canvas.width = 150;
 		canvas.height = 600;
 
+		var canvasCopie = document.getElementById(myCanvasId);
+		var contextCopie = canvas.getContext('2d');
+
+
 		var startTime = (new Date()).getTime();
 		var nbTokens;
 		for (nbTokens = 0; tokens[x + ',' + (rows - nbTokens)]; nbTokens++);
 
 		if(nbTokens < rows) {
-			animate(myCircle, canvas, context, startTime, nbTokens);
+			animate(myCircle, canvasCopie, contextCopie, startTime, nbTokens);
 		}
 	}
 }
