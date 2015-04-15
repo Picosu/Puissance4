95b8c0dd40ca775eb72fabf277099d6831943d67
Maxence de Cussac <maxencedecussac@MBP-de-Maxence.bouyguesbox.fr>
2015-04-15 08:14:09 +0200

    suppression du modèle dans le main

--- js/GameConnect4.js
+++ js/GameConnect4.js
@@ -5,38 +5,6 @@ var rows = 4;
 var columns = 7;
 var tokens = {};
 
-// Token model
-function Token(y, color) {
-	this.x = 75;
-	this.y = y;
-	if (color) {
-		this.color = color;		
-	} else {
-		this.color = "#fbdd12";
-	}
-	this.column = 0;
-	this.radius = 75;
-
-	this.draw = function() {
-		var canvas = document.getElementById("canvas" + this.column);
-		var context = canvas.getContext("2d");
-        
-		context.clearRect(0, 0, canvas.width, canvas.height);
-
-		context.beginPath();
-		context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-
-		context.fillStyle = this.color;
-
-		context.fill();
-		context.lineWidth = 4;
-		context.strokeStyle = "#000000";
-		context.stroke();
-
-		//context.closePath();
-	}
-};
-
 var isAnimated = false;
 
 function drawAllToken(column) {
